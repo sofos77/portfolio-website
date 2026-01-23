9690fd51b2181ff069542617e8c55d26ad9c2b12
sofos77 <s.biadsi@gmail.com>
2026-01-23 15:18:07 +0100

    video compressed and small changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var slideIndex = [1, 1, 1, 1, 1, 1, 1];
-var slideId = ["slide", "slide4", "slide5", "slide6", "slide7", "slide8"];
+var slideIndex = [1, 1, 1, 1, 1, 1, 1, 1];
+var slideId = ["slide", "slide2", "slide3", "slide4", "slide5", "slide6", "slide7", "slide8"];
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
